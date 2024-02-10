1f60f0e91d68402547647c7eaa6446b30dd05305
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-02-10 15:35:43 -0300

    Criar um array de strings no VC.

--- TabViewDemo/ViewController.swift
+++ TabViewDemo/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
+    var strings: [String] = ["Alpha", "Beta", "Ulimited", "Recised"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
