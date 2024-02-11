770c1596bce4316a94c541dee22e9a588fd5fdcf
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2024-02-11 15:22:40 -0300

    Criar um Cocoa Touch Class

---
+++ TabViewDemo/MyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyTableViewCell.swift
+//  TabViewDemo
+//
+//  Created by Rafael Gonzaga on 11/02/24.
+//
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
